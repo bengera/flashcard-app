3ca1c9db8c9143dcc34981c77e8a258d0eb18437
Ben <61493854+bengera@users.noreply.github.com>
2025-12-03 15:43:02 +0700

    Add categories dropdown btn and hide mastered checkbox to flashcard header

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,37 @@
 
 function App() {
   return (
+    <div className="master-container">
     <header className="header">
       <div className="header__content">
         <img src="images\logo-small.svg" alt="logo" className="header__logo" />
-        <div className="header__tab">
+        <div className="header__tab shadow">
           <button className="button-tab active">Study Mode</button>
           <button className="button-tab">All Cards</button>
         </div>
       </div>
     </header>
+    <main>
+    <section className="flashcard-section shadow">
+       <div className="flashcard__content">
+        <div className="flashcard__header">
+          <button className="button-categories">All Categories</button>
+          <div className="flashcard__checkbox-content">
+          <input className="checkbox-basic" type="checkbox" id="hide-mastered" />
+          <label htmlFor="hide-mastered">Hide Mastered
+          </label>
+          </div>
+          {/* <img src="images/icon-chevron-down.svg" alt="arrow" /> */}
+        </div>
+        <div className="divider"></div>
+        <div className="flashcard__container">
+          <div className="flashcard__content"></div>
+        </div>
+
+       </div> {/* flashcard__content end */}
+    </section>
+    </main>
+    </div>
   );
 }
 
