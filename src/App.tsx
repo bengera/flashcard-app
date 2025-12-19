9dcb3c7b88fe873b2b4dd541819606a00a229417
Ben <61493854+bengera@users.noreply.github.com>
2025-12-19 11:46:12 +0700

    Fix icon alignment issue on phones

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ return (
 <div className="app">
     <header className="header">
         <div className="header__content">
-            <img src="images\logo-small.svg" alt="logo" className="header__logo" />
+            <img src="images/logo-small.svg" alt="logo" className="header__logo" />
             <div className="tabs u-shadow">
                 <button  className="tab tab--active u-rounded-pill-narrow">Study Mode</button>
                 <button type="button" className="tab u-rounded-pill-narrow">All Cards</button>
