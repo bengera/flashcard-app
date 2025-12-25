22423e9acc0914a6d25ffabc735ffb18d2c15d01
Ben <61493854+bengera@users.noreply.github.com>
2025-12-25 22:48:05 +0700

    Change statistics heading size

--- src/App.tsx
+++ src/App.tsx
@@ -113,13 +113,12 @@ function App() {
         </section>
 
         <section className="statistics u-shadow">
-          <h2 className="statistic__heading">Study Statistics</h2>
+          <h2 className="statistics__heading">Study Statistics</h2>
             <StatisticCard label="Total Cards" number={40} icon="stats-total" variant="blue"/>
             <StatisticCard label="Mastered" number={11} icon="stats-mastered" variant="teal"/>
             <StatisticCard label="In Progress" number={21} icon="stats-in-progress" variant="red"/>
             <StatisticCard label="Not Started" number={8} icon="stats-not-started" variant="pink"/>
-            
-        
+               
         </section>
       </main>
     </div>
