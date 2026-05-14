3cbe11aab61cba1dfb2f5e353fb1031cd3d3c5b0
Ben <61493854+bengera@users.noreply.github.com>
2026-05-14 13:10:58 +0700

    Remove extracted component filter

--- src/components/study.tsx
+++ src/components/study.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { FlashCardContent } from "./flashcard-content";
 import { EmptyPanel } from "./emptyPanel";
 import type { Flashcard } from "../types/flashcard";
-import { StudyFilters } from "./studyFilters";
 
 type StudyPanelProps = {
   cards: Flashcard[];
@@ -38,10 +37,17 @@ export function StudyPanel({
   const hasCards: boolean = visibleCards.length > 0;
   const currentCard = hasCards ? visibleCards[currentIdx] : undefined;
   const isMastered = currentCard?.knownCount === 5;
-  const [showCategories, setShowCategories] = useState(false); // false by default
+  const [showCatergories, setShowCategories] = useState(false); // false by default
 
   // DROPDOWN CATEGORIES RENDER
-
+  function dropDown() {
+    setShowCategories(!showCatergories);
+    if (!showCatergories) {
+      const categories = cards.map((card) => card.category);
+      const uniqueCategories = [...new Set(categories)];
+      setUniqueCat(uniqueCategories.sort());
+    }
+  }
 
   function nextCard() {
     setReveal(false);
@@ -61,17 +67,25 @@ export function StudyPanel({
     });
   }
 
-  function setShuffledCards(){
-     if (!currentCard) return;
-     setCards((prev) =>
-                prev.map((card) =>
-                  card.id === currentCard.id
-                    ? { ...card, knownCount: Math.min(card.knownCount + 1, 5) }
-                    : card
-                )
-              );
+  function shuffleArray<T>(array: T[]): T[] {
+    const shuffled = [...array];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+
+    return shuffled;
   }
 
+  function filterCategories(category: string, checked: boolean) {
+    setSelectedCategories((prev) => {
+      if (checked) {
+        return [...prev, category];
+      } else {
+        return prev.filter((item) => item !== category);
+      }
+    });
+  }
 
   useEffect(() => {
     if (currentIdx >= visibleCards.length) {
@@ -88,21 +102,65 @@ export function StudyPanel({
   return (
     <section className="study u-shadow">
       <div className="study__header">
-      
-      <StudyFilters
-      cards={cards}
-      hideMasteredCards={hideMasteredCards}
-      setHideMasteredCards={setHideMasteredCards}
-      selectedCategories={selectedCategories}
-      setSelectedCategories={setSelectedCategories}
-      uniqueCat={uniqueCat}
-      setUniqueCat={setUniqueCat}
-      showCategories={showCategories}
-      setShowCategories={setShowCategories}
-      />
-      </div>
-
     
+        <div className="study__filters">
+          <button
+            type="button"
+            className="btn btn--categories u-rounded-pill-narrow"
+            onClick={() => dropDown()}
+          >
+            All Categories
+            <img src="images/icon-chevron-down.svg" alt="arrow-icon" />
+          </button>
+
+          <div className="study__option">
+            <input
+              className="study__checkbox"
+              checked={hideMasteredCards}
+              type="checkbox"
+              id="hide-mastered"
+              onChange={(e) => setHideMasteredCards(e.target.checked)}
+            />
+            <label className="study__label" htmlFor="hide-mastered">
+              Hide Mastered
+            </label>
+          </div>
+
+          {/* RENDERING CATEGORIES */}
+          {showCatergories ? (
+            <div className="study__categories-dropdown">
+              {uniqueCat.map((item) => {
+                const count = cards.filter(
+                  (card) => card.category === item
+                ).length;
+                return (
+                  <div className="category-item" key={item}>
+                    <input
+                      type="checkbox"
+                      className="category-dropdown__checkbox"
+                      onChange={(e) => filterCategories(item, e.target.checked)}
+                      checked={selectedCategories.includes(item)}
+                    />
+                    <p className="category-item__description">{item}</p>
+                    <p className="category-item__number">({count})</p>
+                  </div>
+                );
+              })}
+            </div>
+          ) : null}
+        </div>
+
+        <button
+          type="button"
+          className="btn btn--shuffle u-rounded-pill-narrow"
+          onClick={() => {
+            setCards((prev) => shuffleArray(prev));
+          }}
+        >
+          <img src="images/icon-shuffle.svg" alt="shuffle-icon" />
+          Shuffle
+        </button>
+      </div>
 
       <hr className="solid" />
 
@@ -126,7 +184,15 @@ export function StudyPanel({
           }`}
           onClick={() => {
             if (!currentCard) return;
-            setShuffledCards();
+            {
+              setCards((prev) =>
+                prev.map((card) =>
+                  card.id === currentCard.id
+                    ? { ...card, knownCount: Math.min(card.knownCount + 1, 5) }
+                    : card
+                )
+              );
+            }
           }}
           disabled={isMastered}
         >
