a6060c5488db0440db7bb10b67b1286c0552e52b
Ben <61493854+bengera@users.noreply.github.com>
2026-06-09 13:29:51 +0700

    Add basic elements for mini flashcards

--- src/components/allCards.tsx
+++ src/components/allCards.tsx
@@ -4,95 +4,113 @@ import type React from "react";
 import type { FlashCardControlProps } from "./flashcardControls";
 import { FlashcardControls } from "./flashcardControls";
 
- 
- type cardsStateProps = {
- cardsState: {
+type cardsStateProps = {
+  cardsState: {
     cards: Flashcard[];
     setCards: React.Dispatch<React.SetStateAction<Flashcard[]>>;
+  };
+  flashCardControlsProps: FlashCardControlProps;
 };
-   flashCardControlsProps: FlashCardControlProps;
-
-};
-
 
 export function AllCards({
   cardsState: { setCards },
-  flashCardControlsProps
+  flashCardControlsProps,
 }: cardsStateProps) {
   const [question, setQuestion] = useState<string>("");
   const [answer, setAnswer] = useState<string>("");
   const [category, setCategory] = useState<string>("");
 
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>){
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    console.log(question, answer, category)
+    console.log(question, answer, category);
 
-    const newCard : Flashcard = {
+    const newCard: Flashcard = {
       id: crypto.randomUUID(),
       question,
       answer,
       category,
-      knownCount: 0
-    }
+      knownCount: 0,
+    };
 
-    setCards((prevCards) => [...prevCards, newCard])
-    setQuestion('');
-    setAnswer('');
+    setCards((prevCards) => [...prevCards, newCard]);
+    setQuestion("");
+    setAnswer("");
     // blank space for cateogry to add my questions without retyping
-
   }
 
   return (
     <>
-    <form className="card-form" onSubmit={handleSubmit}>
-  <div className="form-group flex-group">
-    <label htmlFor="question">Question</label>
-    <input
-      id="question"
-      name="question"
-      type="text"
-      placeholder="e.g., What is the capital of France?"
-      value={question}
-      onChange={(e) => setQuestion(e.currentTarget.value)}
-    />
-  </div>
+      <form className="card-form" onSubmit={handleSubmit}>
+        <div className="form-group flex-group">
+          <label htmlFor="question">Question</label>
+          <input
+            id="question"
+            name="question"
+            type="text"
+            placeholder="e.g., What is the capital of France?"
+            value={question}
+            onChange={(e) => setQuestion(e.currentTarget.value)}
+          />
+        </div>
 
-  <div className="form-group flex-group">
-    <label htmlFor="answer">Answer</label>
-    <textarea
-      id="answer"
-      name="answer"
-      placeholder="e.g., Paris"
-      value={answer}
-      onChange={(e) => setAnswer(e.currentTarget.value)}
-    ></textarea>
-  </div>
+        <div className="form-group flex-group">
+          <label htmlFor="answer">Answer</label>
+          <textarea
+            id="answer"
+            name="answer"
+            placeholder="e.g., Paris"
+            value={answer}
+            onChange={(e) => setAnswer(e.currentTarget.value)}
+          ></textarea>
+        </div>
 
-  <div className="form-group flex-group">
-    <label htmlFor="category">Category</label>
-    <input
-      id="category"
-      name="category"
-      type="text"
-      placeholder="e.g., Geography"
-      value={category}
-      onChange={(e) => setCategory(e.currentTarget.value)}
-    />
-  </div>
+        <div className="form-group flex-group">
+          <label htmlFor="category">Category</label>
+          <input
+            id="category"
+            name="category"
+            type="text"
+            placeholder="e.g., Geography"
+            value={category}
+            onChange={(e) => setCategory(e.currentTarget.value)}
+          />
+        </div>
 
-  <button className="btn-submit u-rounded-pill u-shadow--thick" type="submit">
-     <img src="images/icon-circle-plus.svg" alt="icon-plus" />
-    Create Card</button>
-</form>
+        <button
+          className="btn-submit u-rounded-pill u-shadow--thick"
+          type="submit"
+        >
+          <img src="images/icon-circle-plus.svg" alt="icon-plus" />
+          Create Card
+        </button>
+      </form>
 
-<div className="flashcards-container">
-      {/* Flashcard controls component here */}
-      <div className="flashcard-controls">
-        <FlashcardControls {...flashCardControlsProps} />
+      <div className="flashcards-container">
+        {/* Flashcard controls component here */}
+        <div className="flashcard-controls">
+          <FlashcardControls {...flashCardControlsProps} />
+        </div>
+        <main>
+          <div className="flashcards-preview">
+
+            <article className="flashcard-box">
+              <h2 className="flashcard-box__heading">
+                What does HTML stand for?
+              </h2>
+              <p>Answer:</p>
+              <p>HyperText Markup Language</p>
+              <div className="flashcard-box__meta-data">
+                <span>Web Development</span>
+                <progress id="flashcard-progress-bar" max="100" value="70">
+                  0/5
+                </progress>
+                <button>...</button>
+              </div>
+            </article>
+
+          </div> 
+        </main>
       </div>
-</div>
-
-
-</>
+    </>
   );
 }
