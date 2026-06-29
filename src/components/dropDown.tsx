2d9082d102e2bd29719e165d3589c39428041d4b
Ben <61493854+bengera@users.noreply.github.com>
2026-06-29 13:03:38 +0700

    Add function for remove id button

--- src/components/dropDown.tsx
+++ src/components/dropDown.tsx
@@ -1,11 +1,23 @@
-export function DropDown({}){
+type DropDownTypeProps = {
+ cardId: string;
+};
+
+
+
+export function DropDown({cardId}: DropDownTypeProps){
+
+    function removeCard(){
+        console.log('removing card')
+        console.log(cardId)
+    }
+
     return(
         <div className="flashcard-box-dropdown">
                         <button className="flashcard-box-btn-edit">
                           <img src="images/icon-edit.svg" alt="edit button" className="flashcard-box-btn-icon" />
                           Edit
                         </button>
-                        <button className="flashcard-box-btn-delete">
+                        <button className="flashcard-box-btn-delete" onClick={()=> removeCard()}>
                           <img src="images/icon-delete.svg" alt="delete button" className="flashcard-box-btn-icon" />
                           Delete</button>
                       </div>
